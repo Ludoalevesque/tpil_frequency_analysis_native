3d38583791e8a1f975b2e4165b26fa799c0d7fb9
Ludoalevesque <ludovic.arsenault-levesque@polymtl.ca>
2023-08-07 12:21:25 -0400

    Added main.sh

--- main.sh
+++ main.sh
@@ -19,25 +19,25 @@ export brain_prob_mask="${template_dir}/MICCAI2012-Multi-Atlas-Challenge-Data/T_
 export brain_extract_mask="${template_dir}/MICCAI2012-Multi-Atlas-Challenge-Data/T_template0_BrainCerebellumRegistrationMask.nii.gz"
 export brain_extract_prefix="${output_dir}/BrainExtraction/sub-${sub}_T1"
 
-# # Start the container
-# docker run --rm \
-#     -v "${root_dir}:${root_dir}" \
-#     antsx/ants \
-#     bash antsBrainExtraction.sh -d 3 -a "$T1" -e "$template_with_skull" -m "$brain_prob_mask" -f "$brain_extract_mask" -o "${brain_extract_prefix}"
+# Start the container
+docker run --rm \
+    -v "${root_dir}:${root_dir}" \
+    antsx/ants \
+    bash antsBrainExtraction.sh -d 3 -a "$T1" -e "$template_with_skull" -m "$brain_prob_mask" -f "$brain_extract_mask" -o "${brain_extract_prefix}"
 
 
 
-# # 2- Segment the brain extracted T1 subcortical structures
+# 2- Segment the brain extracted T1 subcortical structures
 
 export T1_brain="${brain_extract_prefix}_BrainExtractionBrain.nii.gz"
 export segmentation_dir="${output_dir}/Segmentation"
 
-# if [ ! -d "$segmentation_dir" ]; then
-#   mkdir -p "$segmentation_dir"
-# fi
+if [ ! -d "$segmentation_dir" ]; then
+  mkdir -p "$segmentation_dir"
+fi
 
-# run_first_all -i $T1_brain -o "${segmentation_dir}/sub-${sub}" -b
-# echo "T1 segmentation done."
+run_first_all -i $T1_brain -o "${segmentation_dir}/sub-${sub}" -b
+echo "T1 segmentation done."
 
 
 
@@ -47,12 +47,12 @@ input_fmri_file="${func_dir}/sub-${sub}_task-rest_bold.nii.gz"
 bold_out_dir="${output_dir}/BOLD"
 HMC_bold="${bold_out_dir}/sub-${sub}_task-rest_bold_HMC.nii.gz"
 
-# if [ ! -d "$bold_out_dir" ]; then
-#   mkdir -p "$bold_out_dir"
-# fi
+if [ ! -d "$bold_out_dir" ]; then
+  mkdir -p "$bold_out_dir"
+fi
 
-# mcflirt -in "$input_fmri_file" -out "$HMC_bold" -mats -plots
-# echo "Head motion correction applied and saved."
+mcflirt -in "$input_fmri_file" -out "$HMC_bold" -mats -plots
+echo "Head motion correction applied and saved."
 
 
 # 4- Compute the bold ref to be used in T1 to bold registration
@@ -60,17 +60,17 @@ HMC_bold="${bold_out_dir}/sub-${sub}_task-rest_bold_HMC.nii.gz"
 export bold_ref_file="${bold_out_dir}/sub-${sub}_task-rest_boldref.nii.gz"
 half_vol_file="half_vol_file.nii.gz"
 
-# # Task 1: Get the volume at 0.5
-# num_volumes=$(fslinfo "$HMC_bold" | grep '^dim4' | awk '{print $2}')
-# half_index=$((num_volumes / 2))
-# fslroi "$HMC_bold" "$half_vol_file" "$half_index" 1
+# Task 1: Get the volume at 0.5
+num_volumes=$(fslinfo "$HMC_bold" | grep '^dim4' | awk '{print $2}')
+half_index=$((num_volumes / 2))
+fslroi "$HMC_bold" "$half_vol_file" "$half_index" 1
 
-# # Task 2: Apply brain extraction on the ref volume image
-# bet "$half_vol_file" "$bold_ref_file"
-# echo "Brain extraction applied and bold ref saved."
+# Task 2: Apply brain extraction on the ref volume image
+bet "$half_vol_file" "$bold_ref_file"
+echo "Brain extraction applied and bold ref saved."
 
-# # Removing temporary files
-# rm "$half_vol_file"
+# Removing temporary files
+rm "$half_vol_file"
 
 
 # 5- Register T1 to Bold 
@@ -81,10 +81,10 @@ if [ ! -d "$registration_dir" ]; then
 fi
 
 
-# docker run --rm \
-#     -v "${root_dir}:${root_dir}" \
-#     antsx/ants \
-#     bash antsRegistrationSyNQuick.sh -d 3 -f "$bold_ref_file" -m "$T1_brain" -o "${registration_dir}/sub-${sub}_T1_space-BOLD_"
+docker run --rm \
+    -v "${root_dir}:${root_dir}" \
+    antsx/ants \
+    bash antsRegistrationSyNQuick.sh -d 3 -f "$bold_ref_file" -m "$T1_brain" -o "${registration_dir}/sub-${sub}_T1_space-BOLD_"
 
 
 # 6- Register the segmentation to BOLD space
